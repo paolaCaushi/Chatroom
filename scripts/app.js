4d2036e4799c071ab4909364a878e6f7b48a10db
paolaCaushi <pcaushi@unitsystem.net>
2022-10-19 13:08:50 +0200

    updating room

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,8 @@ const chatList = document.querySelector(".chat-list");
 const newChatForm = document.querySelector(".new-chat");
 const newNameForm = document.querySelector('.new-name');
 const updateMssg = document.querySelector('.update-mssg');
+const rooms = document.querySelector('.chat-rooms');
+
 //add a new chat
 newChatForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -25,7 +27,14 @@ newNameForm.addEventListener('submit', e =>{
     updateMssg.innerText = `Your name was updated to ${newName}`;
     setTimeout(()=> updateMssg.innerText= '', 3000);
 });
-
+//update the chat room
+rooms.addEventListener('click', e=>{
+  if(e.target.tagName === 'BUTTON'){
+    chatUI.clear();
+    chatroom.updateRoom(e.target.getAttribute('id'));
+    chatroom.getChats(chat => chatUI.render(chat));
+  }
+});
 //check local storage for a name
 const username = localStorage.username ? localStorage.username : 'anon';
 //class instances
