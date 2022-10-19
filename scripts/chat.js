aae2efa461a1301efc983de8dbeb6733bc061795
paolaCaushi <pcaushi@unitsystem.net>
2022-10-19 11:56:36 +0200

    update room and name

--- scripts/chat.js
+++ scripts/chat.js
@@ -8,6 +8,7 @@ class Chatroom {
     this.room = room;
     this.username = username;
     this.chats = db.collection("chat");
+    this.unsub;
   }
   async addChat(message) {
     //format a chat obj
@@ -22,10 +23,43 @@ class Chatroom {
     const response = await this.chats.add(chat);
     return response;
   }
+  getChats(callback) {
+    this.unsub = this.chats
+      .where("room", "==", this.room)
+      .orderBy("created_at")
+      .onSnapshot((snapshot) => {
+        snapshot.docChanges().forEach((change) => {
+          if (change.type === "added") {
+            //update ui
+            callback(change.doc.data());
+          }
+        });
+      });
+  }
+
+  updateName(username) {
+    this.username = username;
+  }
+  updateRoom(room) {
+    this.room = room;
+    console.log("room updated");
+    if (this.unsub) {
+      this.unsub();
+    }
+  }
 }
 
-const chatroom = new Chatroom("gaming", "shaun");
-chatroom
-  .addChat("hello everyone")
-  .then(() => console.log("chat added"))
-  .catch((err) => console.log(err));
+const chatroom = new Chatroom("general", "shaun");
+
+chatroom.getChats((data) => {
+  console.log(data);
+});
+
+setTimeout(() => {
+  chatroom.updateRoom("gaming");
+  chatroom.updateName("yoshi");
+  chatroom.getChats((data) => {
+    console.log(data);
+  });
+  chatroom.addChat("hello");
+}, 3000);
