d1ea00ac9a097e8494e44fbe45ba075aa14b0f08
paolaCaushi <pcaushi@unitsystem.net>
2022-10-19 12:55:49 +0200

    updating username

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,8 @@
 //dom queries
 const chatList = document.querySelector(".chat-list");
 const newChatForm = document.querySelector(".new-chat");
-
+const newNameForm = document.querySelector('.new-name');
+const updateMssg = document.querySelector('.update-mssg');
 //add a new chat
 newChatForm.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -11,9 +12,26 @@ newChatForm.addEventListener("submit", (e) => {
     .then(() => newChatForm.reset())
     .catch((err) => console.log(err));
 });
+
+//update username
+newNameForm.addEventListener('submit', e =>{
+    e.preventDefault();
+    //update name via chatroom
+    const newName = newNameForm.name.value.trim();
+    chatroom.updateName(newName);
+    //reset form
+    newNameForm.reset();
+    //show then hide the update message
+    updateMssg.innerText = `Your name was updated to ${newName}`;
+    setTimeout(()=> updateMssg.innerText= '', 3000);
+});
+
+//check local storage for a name
+const username = localStorage.username ? localStorage.username : 'anon';
 //class instances
 const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom("general", "shaun");
 
 //get chats and render
 chatroom.getChats((data) => chatUI.render(data));
+
