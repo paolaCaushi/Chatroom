1364cd3214b9246d4f781511d84b4c1a7049f46a
paolaCaushi <pcaushi@unitsystem.net>
2022-10-19 12:30:05 +0200

    sending new chats

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,19 @@
 //dom queries
-const chatList = document.querySelector('.chat-list');
+const chatList = document.querySelector(".chat-list");
+const newChatForm = document.querySelector(".new-chat");
+
+//add a new chat
+newChatForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const message = newChatForm.message.value.trim();
+  chatroom
+    .addChat(message)
+    .then(() => newChatForm.reset())
+    .catch((err) => console.log(err));
+});
 //class instances
 const chatUI = new ChatUI(chatList);
 const chatroom = new Chatroom("general", "shaun");
 
 //get chats and render
 chatroom.getChats((data) => chatUI.render(data));
-
